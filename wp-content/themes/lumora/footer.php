96680fe7085aaf7ae31adab2af21e5f77ea07d7e
Md Khan <mkhan@baycove.org>
2025-01-03 23:20:31 -0500

    Add primary and footer sidebars

--- wp-content/themes/lumora/footer.php
+++ wp-content/themes/lumora/footer.php
@@ -1,21 +1,30 @@
 <?php
 /**
- * The footer template for the Tecklynx theme
+ * The footer template for the Lumora theme.
  *
- * This template displays all of the <head> section and everything up until the opening of the main content area.
- * It is included on all pages using the get_header() WordPress function.
+ * Displays the footer content and widget area.
  *
  * @package Lumora
  * @since Lumora 1.0
  */
 ?>
 
-<footer>Footer</footer>
+<footer id="colophon" role="contentinfo">
+    <div class="container">
+        <h3>Footer</h3>
+
+        <?php if ( is_active_sidebar( 'footer-widget' ) ) : ?>
+            <div class="footer-widgets">
+                <?php dynamic_sidebar( 'footer-widget' ); ?>
+            </div>
+        <?php else : ?>
+            <p><?php esc_html_e( 'Add widgets to the footer from the Widgets panel in WordPress.', 'lumora' ); ?></p>
+        <?php endif; ?>
+    </div>
+</footer>
+
 </div><!-- ./site -->
 </div><!-- ./site-content -->
-
-
-
 
 <?php wp_footer(); ?>
 </body>
