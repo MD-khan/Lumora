07d1408f28e889b35c78f14fb2d2a2150dbf2807
Md Khan <md.monir.khan707@gmail.com>
2025-01-07 23:14:24 -0500

    Setup Webpack for theme development, added configuration and assets.

---
+++ wp-content/themes/lumora/assets/build/js/main.js
@@ -0,0 +1,32 @@
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (function() { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/js/main.js":
+/*!************************!*\
+  !*** ./src/js/main.js ***!
+  \************************/
+/***/ (function() {
+
+eval("//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiLi9zcmMvanMvbWFpbi5qcyIsIm5hbWVzIjpbXSwic291cmNlUm9vdCI6IiIsInNvdXJjZXMiOlsid2VicGFjazovL2x1bW9yYS8uL3NyYy9qcy9tYWluLmpzPzkyOTEiXSwic291cmNlc0NvbnRlbnQiOlsiIl0sIm1hcHBpbmdzIjoiIiwiaWdub3JlTGlzdCI6W119\n//# sourceURL=webpack-internal:///./src/js/main.js\n");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval-source-map devtool is used.
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./src/js/main.js"]();
+/******/ 	
+/******/ })()
+;
---
+++ wp-content/themes/lumora/assets/webpack.config.js
@@ -0,0 +1,126 @@
+/**
+ * Webpack configuration for Lumora theme assets.
+ */
+
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
+
+const isProduction = process.env.NODE_ENV === "production";
+
+// Paths
+const JS_DIR = path.resolve(__dirname, "src/js");
+const IMG_DIR = path.resolve(__dirname, "src/img");
+const SASS_DIR = path.resolve(__dirname, "src/sass");
+const BUILD_DIR = path.resolve(__dirname, "build");
+
+module.exports = {
+  // Entry points for JavaScript
+  entry: {
+    main: JS_DIR + "/main.js",
+    editor: JS_DIR + "/editor.js"
+  },
+
+  // Output configuration
+  output: {
+    path: BUILD_DIR,
+    filename: "js/[name].js"
+  },
+
+  // Source maps
+  devtool: isProduction ? "source-map" : "eval-source-map",
+
+  // Optimization settings
+  optimization: {
+    minimize: isProduction,
+    minimizer: [
+      new TerserPlugin({
+        parallel: true,
+        terserOptions: {
+          format: {
+            comments: false
+          }
+        },
+        extractComments: false
+      }),
+      new CssMinimizerPlugin()
+    ]
+  },
+
+  // Loaders
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        include: [JS_DIR],
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"]
+          }
+        }
+      },
+      {
+        test: /\.(scss|css)$/,
+        include: [SASS_DIR],
+        use: [
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+          {
+            loader: "postcss-loader",
+            options: {
+              postcssOptions: {
+                plugins: [
+                  require("autoprefixer")
+                ]
+              }
+            }
+          },
+          "sass-loader"
+        ]
+      },
+      {
+        test: /\.(png|jpg|jpeg|gif|svg|ico)$/,
+        type: "asset/resource",
+        generator: {
+          filename: "images/[name][ext]"
+        }
+      },
+      {
+        test: /\.(woff(2)?|eot|ttf|otf|svg)$/,
+        type: "asset/resource",
+        generator: {
+          filename: "fonts/[name][ext]"
+        }
+      }
+    ]
+  },
+
+  // Plugins
+  plugins: [
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({
+      filename: "css/[name].css"
+    }),
+    new CopyPlugin({
+      patterns: [
+        { from: IMG_DIR, to: BUILD_DIR + "/images" }
+      ]
+    })
+  ],
+
+  // Development server
+  devServer: {
+    static: {
+      directory: BUILD_DIR
+    },
+    compress: true,
+    port: 9000,
+    open: true,
+    hot: true
+  }
+};
